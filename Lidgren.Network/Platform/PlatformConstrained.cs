567d8546c620457be898a4da7ef4681cfa974575
Justin <robojoos@gmail.com>
2016-07-27 21:55:29 -0700

    need unconstrained to work on unity linux

--- Lidgren.Network/Platform/PlatformConstrained.cs
+++ Lidgren.Network/Platform/PlatformConstrained.cs
@@ -29,9 +29,9 @@ namespace Lidgren.Network
 		public static IPAddress GetMyAddress(out IPAddress mask)
 		{
 			mask = null;
+#if UNITY_ANDROID || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_IOS || UNITY
 			try
 			{
-#if UNITY_ANDROID || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_IOS || UNITY
 				if (!(UnityEngine.Application.internetReachability == UnityEngine.NetworkReachability.NotReachable))
 				{
 					return null;
@@ -43,7 +43,7 @@ namespace Lidgren.Network
 				return null;
 			}
 #endif
-			return null;
+            return null;
 		}
 
 		public static byte[] GetMacAddressBytes()
